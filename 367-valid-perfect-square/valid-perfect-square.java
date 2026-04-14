295c608fdc1f44eb1c09ffd22b084f0991b7e881
vishnu-yt687 <dvishnuvardhan844@gmail.com>
2026-04-14 15:04:48 +0530

    Time: 1855 ms (5.00%) | Memory: 41.3 MB (99.79%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num<2){
+                return true;
+        }
+        for(int i=0;i<=num/2;i++){
+            if(i*i==num){
+                return true;
+            }
+            if(i*i>num){
+                return false;
+            }
+        }
+        return false;
+    }
+}
